6a1604b5b8257d60f4a3712786a76fb9f7a190c2
jsuci <jsuci.jsuci@gmail.com>
2022-09-11 23:55:01 +0800

    fix saving data to csv file

--- index.js
+++ index.js
@@ -39,40 +39,106 @@ async function startBot() {
 
     // global functions
     await page.exposeFunction("filterDuplicate", filterDuplicate);
-    await page.exposeFunction("readCSV", readCSV);
+    await page.exposeFunction("readCleanCSV", readCleanCSV);
     await page.exposeFunction("saveCSV", saveCSV);
 
     const twitterPages = [
+        // "https://twitter.com/anonimoXD1_2_3",
+        // "https://twitter.com/wojackol",
+        // "https://twitter.com/Hoonyrock_me"
+        "https://twitter.com/Todayspinlinks",
+        "https://twitter.com/Arima17071520",
+        "https://twitter.com/donatebtc2019",
+        "https://twitter.com/coinmastercard",
+        "https://twitter.com/coinmaster1978",
+        "https://twitter.com/coinmaster2022f",
+        "https://twitter.com/CoinmasterSpi20",
+        "https://twitter.com/coinmastergifts",
+        "https://twitter.com/CoinMas46556769",
+        "https://twitter.com/link_SPINS",
         "https://twitter.com/BentalbSimo",
-        "https://twitter.com/michelneri1",
-        "https://twitter.com/SandersDemonta",
-        "https://twitter.com/gemsbrawlstars2",
-        "https://twitter.com/coinmas08077718",
+        "https://twitter.com/spinmas55376377",
+        "https://twitter.com/RewardSpinLink",
+        "https://twitter.com/CoinMasterShare",
+        "https://twitter.com/Wins_Spins",
+        "https://twitter.com/CoinMasterUS",
+        "https://twitter.com/TiradasGratisCM",
+        "https://twitter.com/Zara46742541",
+        "https://twitter.com/FreeCoin_Master",
+        "https://twitter.com/coinmaster_tool",
+        "https://twitter.com/spinscheats",
+        "https://twitter.com/coinmaster30",
+        "https://twitter.com/makeMon58968772",
+        "https://twitter.com/Generatorgames2",
+        "https://twitter.com/sreyno36468144",
+        "https://twitter.com/ThetanArenaN",
         "https://twitter.com/spinsandcoins",
-        "https://twitter.com/Richard95739988",
-        "https://twitter.com/jollypeter7",
-        "https://twitter.com/miketoto14",
-        "https://twitter.com/simonju42561140",
-        "https://twitter.com/stevenm43732671",
+        "https://twitter.com/freespin1k",
+        "https://twitter.com/freespin_2022",
+        "https://twitter.com/claimspin",
+        "https://twitter.com/coinmasterxyz",
+        "https://twitter.com/freespincoins",
+        "https://twitter.com/DailySpins1",
+        "https://twitter.com/Freesspinslinks",
+        "https://twitter.com/coinmaster55165",
+        "https://twitter.com/weirdnews2k",
+        "https://twitter.com/coin_masterfree",
+        "https://twitter.com/CoinMas83793700",
+        "https://twitter.com/coinmasterusa2",
+        "https://twitter.com/cmfree70spinli1",
+        "https://twitter.com/CoinUnlimited",
+        "https://twitter.com/CoinOfficail",
+        "https://twitter.com/6djchandu",
+        "https://twitter.com/freespinlink",
+        "https://twitter.com/CMClaimSpins",
+        "https://twitter.com/CoinMasterGamer",
+        "https://twitter.com/cmspinsdaily",
+        "https://twitter.com/links_coin",
+        "https://twitter.com/coinmaster_spin",
+        "https://twitter.com/CoinMasterLinks",
+        "https://twitter.com/coinmastertweet",
+        "https://twitter.com/daily_spins",
+        "https://twitter.com/coin_masterss",
+        "https://twitter.com/coinmaster_spn",
+        "https://twitter.com/coinmasterspint",
         "https://twitter.com/coinmaster09",
+        "https://twitter.com/coinmasterspind",
+        "https://twitter.com/Coinmasterspi9",
+        "https://twitter.com/coinmasterspins",
+        "https://twitter.com/coinmaster222",
+        "https://twitter.com/FloranceOlman",
+        "https://twitter.com/Spingift1",
+        "https://twitter.com/coinmaster_link",
+        "https://twitter.com/sokly72168231",
+        "https://twitter.com/Free_Spinss",
+        "https://twitter.com/garden_scapes",
+        "https://twitter.com/CoinMas50294009",
+        "https://twitter.com/CoinMasterGame",
+        "https://twitter.com/RewardSpinLink"
     ]
 
-    await login(page);
+    try {
+        await login(page, browser);
 
-    for (const ePage of twitterPages) {
-        await getFollowers(page, ePage)
-
+        for (const ePage of twitterPages) {
+            await getFollowers(page, ePage)
+        }
+        
+        console.log('taking screenshot.')
+        await delay(5000);
+        await page.screenshot({ path: 'test-screenshot.jpg' , type: 'jpeg' });
+    } catch (e) {
+        console.log(`error: ${e}`);
+        console.log('restarting bot.')
+        await restartBot(browser);
     }
-    
-    console.log('taking screenshot.')
-    await delay(5000);
-    await page.screenshot({ path: 'test-screenshot.jpg' , type: 'jpeg' });
+
 
 }
 
 async function getFollowers(page, url) {
 
-    console.log(`opening ${url} page.`)
+    console.log(`\n\nopening ${url} page.`)
     await page.goto(url, {
         waitUntil: 'load',
     });
@@ -82,6 +148,8 @@ async function getFollowers(page, url) {
         timeout: 60000,
         visible: true,
     }).then(async (element) => {
+
+        // change div:nth-child(1) to switch from followers to following
         const followersCount = await page.$eval("div:nth-child(2) > a[role='link'] > span > span", (el) => {
             function convertToInt(num) {
                 let value = 0;
@@ -103,9 +171,10 @@ async function getFollowers(page, url) {
         console.log(`total followers: ${followersCount}`);
 
         console.log("clicking followers list.")
+        // change div:nth-child(1) to switch from followers to following
         await page.click("div:nth-child(2) > a[role='link'] > span > span")
-        console.log("5sec delay.")
-        await delay(5000)
+        console.log("10sec delay.")
+        await delay(10000)
 
         await page.waitForSelector("div[data-testid='cellInnerDiv']", {
             timeout: 60000,
@@ -113,60 +182,89 @@ async function getFollowers(page, url) {
         }).then(async () => {
 
             console.log("getting followers.");
-            const followers = await page.evaluate((followersCount) => {
+            let allFollowers = []
 
-                async function getIDLInk(followersCount) {
-                    let entries = []
+            while (allFollowers.length < followersCount) {
 
-                    while (await new Promise(resolve => setTimeout(() => resolve(entries.length), 20000)) <= followersCount) {
+                await new Promise((resolve, reject) => {
+                    setTimeout(async () => {
+                        // scroll page, get partial results, log partial results, append to allFollowers
+                        const partialFollowers = await page.evaluate(async () => {
 
-                        await Promise.all([
-                            setTimeout(() => {
-                                console.log("scroll page.");
-                                window.scrollTo(0, window.document.body.scrollHeight)
-                            }, 3000),
-                            setTimeout(() => {
-                                console.log("get cells.");
-                                let cells = document.querySelectorAll("div[data-testid='cellInnerDiv']");
-                                for (let i = 0; i < (cells.length - 1); i++) {
-                                    const idLink = cells[i].querySelectorAll(
-                                        "div:nth-child(1) > a[role='link'][tabindex='-1']")[1];
-                                    const twitterID = idLink.innerText;
-                                    const twitterLink = idLink.href;
-        
-                                    entries.push({id: twitterID, link: twitterLink});
-                                }
+                            let entries = []
 
-                            }, 9000),
-                        ])
-                        
-                        console.log(entries);
-                    }
+                            const promise1 = new Promise((resolve, reject) => {
+                                setTimeout(() => {
+                                    console.log("scroll page.");
+                                    window.scrollTo(0, window.document.body.scrollHeight)
+                                    resolve(true);
+                                }, 3000);
+                            });
 
-                    return filterDuplicate(entries)
-                    
-                }
+                            const promise2 = new Promise((resolve, reject) => {
+                                setTimeout(() => {
+                                    console.log("get cells.");
+                                    let cells = document.querySelectorAll("div[data-testid='cellInnerDiv']");
+                                    let temp = []
+                                    for (let i = 0; i < (cells.length - 1); i++) {
+                                        const idLink = cells[i].querySelectorAll(
+                                            "div:nth-child(1) > a[role='link'][tabindex='-1']")[1];
+                                        const twitterID = idLink.innerText;
+                                        const twitterLink = idLink.href;
 
-                return getIDLInk(followersCount);
+                                        if (twitterID !== undefined) {
+                                            temp.push({id: twitterID, link: twitterLink});
+                                        }
+                                    }
 
+                                    resolve(temp)
+                                }, 6000);
+                            });
 
-            }, followersCount);
+                            await Promise.all([
+                                promise1,
+                                promise2                   
+                            ]).then((values) => {
 
-            console.log(`total followers gathered: ${followers.length}`);
-            await saveCSV("followers.csv", followers);
+                                entries.push(...values[1])
 
-            console.log("\n\n")
+                            }) //end Promise.all()
+
+                            return entries
+
+                        }) // end page.evaluate()
+
+                        resolve(partialFollowers)
+
+                    }, 10000)
+                }).then(async (value) => {
+
+                    allFollowers = allFollowers.concat(value)
+                    console.log(`${allFollowers.length} out of ${followersCount} completed`)
+
+                    console.log(`saving partial followers\n`)
+                    await saveCSV('followers.csv', value, true)
+
+                })
+            } // end while
+
+            console.log('cleaning followers.csv')
+            const csvEntries = await readCleanCSV('followers.csv')
+            console.log(`total saved followers: ${csvEntries.length}`)
+            await saveCSV('followers.csv', csvEntries, false)
+
 
         })
     })
 
 }
 
-async function readCSV(fileName) {
+async function readCleanCSV(fileName) {
     let results = []
 
     try {
-        let json = csvToJson.getJsonFromCsv(fileName);
+        let json = csvToJson.fieldDelimiter(',').getJsonFromCsv(fileName);
+
         for(let i = 0; i < json.length; i++) {
             results.push(json[i]);
         }
@@ -174,104 +272,96 @@ async function readCSV(fileName) {
         console.log("csv not found.")
     }
 
-    return results;
+    return filterDuplicate(results);
 }
 
-async function saveCSV(fileName, lst) {
-    console.log("reading previous data.")
-    let prevFollowers = await readCSV(fileName)
+async function saveCSV(fileName, lst, append) {
 
-    console.log("merging new data.")
-    let mergeFollowers = prevFollowers.concat(lst);
-    console.log(`total merged: ${mergeFollowers.length}`)
+    // let prevFollowers = await readCSV(fileName)
 
-    console.log("filtering data.")
-    let filteredFollowers = await filterDuplicate(mergeFollowers)
-    console.log(`total filtered: ${filteredFollowers.length}`)
+    // console.log("merging new data.")
+    // let mergeFollowers = prevFollowers.concat(lst);
+    // console.log(`total merged: ${mergeFollowers.length}`)
 
-    console.log("saving data.")
-    const csv = new ObjectsToCsv(filteredFollowers);
-    await csv.toDisk(`./${fileName}`, {append: true});
+    // console.log("filtering data.")
+    // let filteredFollowers = await filterDuplicate(mergeFollowers)
+    // console.log(`total filtered: ${filteredFollowers.length}`)
+
+    // console.log("saving data.")
+    const csv = new ObjectsToCsv(lst);
+    await csv.toDisk(`./${fileName}`, {append: append});
+
+    // console.log('\n')
 }
 
 async function filterDuplicate(arr) {
     const results =  Array.from(new Set(arr.map(x => x.id))).map(id => {
+
+        if (id !== undefined) {
             return {
                 id: id,
                 link: arr.find(s => s.id === id).link
             }
-        })
+        } else {
+            return {}
+        }
+    })
 
-        return results;
+    return results;
 }
 
 async function login(page) {
-    try {
 
-        // login
-        await page.goto('https://mobile.twitter.com/i/flow/login', {
-            waitUntil: 'networkidle0',
-        });
+    // login
+    await page.goto('https://mobile.twitter.com/i/flow/login', {
+        waitUntil: 'networkidle0',
+    });
 
-        await page.waitForSelector("input[name='text']", {
-            timeout: 60000,
-            visible: true,
-        }).then(async () => {
-            console.log('typing username.')
-            await page.type("input[name='text']", 'coinmasterfan6', {delay: 100})
+    await page.waitForSelector("input[name='text']", {
+        timeout: 60000,
+        visible: true,
+    }).then(async () => {
+        console.log('typing username.')
+        await page.type("input[name='text']", 'coinmasterfan6', {delay: 100})
 
-            console.log('clicking next.')
-            const nextButton = await page.$x("//*[text()='Next']")
-            await Promise.all([
-                nextButton[0].click(),
-                page.waitForSelector("input[name='password']")
-            ])
+        console.log('clicking next.')
+        const nextButton = await page.$x("//*[text()='Next']")
+        await Promise.all([
+            nextButton[0].click(),
+            page.waitForSelector("input[name='password']")
+        ])
 
-            console.log('typing password.')
-            await page.type("input[name='password']", 'twitter-Jsuci#0', {delay: 100})
+        console.log('typing password.')
+        await page.type("input[name='password']", 'twitter-Jsuci#0', {delay: 100})
 
-            console.log('clicking log in.')
-            const loginButton = await page.$x("//*[text()='Log in']")
-            await Promise.all([
-                loginButton[0].click(),
-            ])
+        console.log('clicking log in.')
+        const loginButton = await page.$x("//*[text()='Log in']")
+        await Promise.all([
+            loginButton[0].click(),
+        ])
 
-        });
+    });
 
-        // home page
-        await page.waitForXPath("//*[text()='Next']", {
-            timeout: 30000,
-            visible: true,
-        }).then(async () => {
-            console.log('already logged in.')
-            const homePage = await page.$x("//*[text()='Next']");
+    // home page
+    await page.waitForXPath("//*[text()='Next']", {
+        timeout: 30000,
+        visible: true,
+    }).then(async () => {
+        console.log('already logged in.')
+        const homePage = await page.$x("//*[text()='Next']");
 
-            console.log('clicking next.')
-            await Promise.all([
-                homePage[0].click(),
-                page.waitForXPath("//*[text()='Home']")
-            ])
-        }).catch(async () => {
+        console.log('clicking next.')
+        await Promise.all([
+            homePage[0].click(),
+            page.waitForXPath("//*[text()='Home']")
+        ])
+    }).catch(async () => {
+        await delay(5000);
+    })
 
-            await delay(5000);
-            // await page.waitForXPath("//*[text()='Home']", {
-            //     timeout: 60000,
-            //     visible: true,
-            // }).then(()=> {
-            //     console.log('home page loaded.\n\n')
-            // })
-        })
-
-        console.log('home page loaded.')
-        console.log("10sec delay.\n\n")
-        await delay(10000);
-
-
-    } catch (e) {
-        console.log(`error: ${e}`);
-        console.log('restarting bot.')
-        await restartBot(browser);
-    }
+    console.log('home page loaded.')
+    console.log("10sec delay.\n\n")
+    await delay(10000);
 
 }
 
